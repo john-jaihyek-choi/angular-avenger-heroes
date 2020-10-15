c8e0038a1eec8e63d300ee86197deba383f2dab5
john-jaihyek-choi <jaihyek.choi@gmail.com>
2020-10-15 10:33:06 -0700

    bulk uploaded entire repo for demo purpose

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 2, name: 'Captain America' },
+  { id: 3, name: 'Spider Man' },
+  { id: 4, name: 'Hulk' },
+  { id: 5, name: 'Thor' },
+  { id: 6, name: 'Black Widow' },
+  { id: 7, name: 'Starlord' },
+  { id: 8, name: 'Groot' },
+  { id: 9, name: 'Dr Strange' },
+  { id: 10, name: 'Ant Man' },
+  { id: 11, name: 'Black Panther' }
+]
